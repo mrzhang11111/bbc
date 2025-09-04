0d7bbf0bfa8a360a18677447154a6c48de4b2381
baba698 <luanoach@gmail.com>
2025-09-04 16:06:57 +0800

    7

---
+++ build.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const { execSync } = require('child_process');
+const path = require('path');
+
+console.log('Building API package...');
+execSync('yarn workspace @animeflix/api build', { stdio: 'inherit', cwd: __dirname });
+
+console.log('Building frontend package...');
+execSync('yarn workspace @animeflix/frontend build', { stdio: 'inherit', cwd: __dirname });
+
+console.log('Build completed successfully!');
